19e9a67e37964154996eceb8900f58af76c2dcb5
Jiajun-github-io <1372207033@qq.com>
2023-01-04 15:58:40 +1030

    Transaction Management

--- src/main/java/io/github/jiajun2001/community/community/service/AlphaService.java
+++ src/main/java/io/github/jiajun2001/community/community/service/AlphaService.java
@@ -1,11 +1,26 @@
 package io.github.jiajun2001.community.community.service;
 
 import io.github.jiajun2001.community.community.dao.AlphaDao;
+import io.github.jiajun2001.community.community.dao.DiscussPostMapper;
+import io.github.jiajun2001.community.community.dao.UserMapper;
+import io.github.jiajun2001.community.community.entity.DiscussPost;
+import io.github.jiajun2001.community.community.entity.User;
+import io.github.jiajun2001.community.community.util.CommunityUtil;
+import jakarta.activation.CommandInfo;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.Date;
 
 @Service
 @Scope("singleton") // can be changed by "prototype"
@@ -13,6 +28,15 @@ public class AlphaService {
 
     @Autowired
     private AlphaDao alphaDao;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
 
     public AlphaService() {
         System.out.println("Construct AlphaService");
@@ -31,4 +55,64 @@ public class AlphaService {
     public void destroy() {
         System.out.println("Destroy AlphaService");
     }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public Object save1() {
+        // Add a new user
+        User user = new User();
+        user.setUsername("alpha");
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+        user.setEmail("alpha@qq.com");
+        user.setHeaderURL("http://image.newcoder.com/head/99t.png");
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        // Add a new post
+        DiscussPost discussPost = new DiscussPost();
+        discussPost.setUserId(user.getId());
+        discussPost.setTitle("Hello");
+        discussPost.setContent("NEW NEW USER");
+        discussPost.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(discussPost);
+
+        // Create an error, see if it can roll back
+        Integer.valueOf("abc");
+
+        return "ok";
+    }
+
+    public Object save2() {
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        return transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus status) {
+                // Add a new user
+                User user = new User();
+                user.setUsername("beta");
+                user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+                user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+                user.setEmail("beta@qq.com");
+                user.setHeaderURL("http://image.newcoder.com/head/999t.png");
+                user.setCreateTime(new Date());
+                userMapper.insertUser(user);
+
+                // Add a new post
+                DiscussPost discussPost = new DiscussPost();
+                discussPost.setUserId(user.getId());
+                discussPost.setTitle("Hola");
+                discussPost.setContent("NEW NEW USER USER");
+                discussPost.setCreateTime(new Date());
+                discussPostMapper.insertDiscussPost(discussPost);
+
+                // Create an error, see if it can roll back
+                Integer.valueOf("abc");
+
+                return "ok";
+            }
+        });
+    }
+
+
 }
---
+++ src/test/java/io/github/jiajun2001/community/community/TransactionTest.java
@@ -0,0 +1,26 @@
+package io.github.jiajun2001.community.community;
+
+import io.github.jiajun2001.community.community.service.AlphaService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class TransactionTest {
+    @Autowired
+    private AlphaService alphaService;
+
+    @Test
+    public void testSave1() {
+        Object obj = alphaService.save1();
+        System.out.println(obj);
+    }
+
+    @Test
+    public void testSave2() {
+        Object obj = alphaService.save2();
+        System.out.println(obj);
+    }
+}
