5a57911c966ee85f385ba0a960cd2bad7fbe1bd3
Jiajun-github-io <1372207033@qq.com>
2023-01-11 23:19:12 +1030

    Implement reply to posts / comments / users

--- src/main/java/io/github/jiajun2001/community/community/dao/DiscussPostMapper.java
+++ src/main/java/io/github/jiajun2001/community/community/dao/DiscussPostMapper.java
@@ -16,4 +16,6 @@ public interface DiscussPostMapper {
     int insertDiscussPost(DiscussPost discussPost);
 
     DiscussPost selectDiscussPostById(int id);
+
+    int updateCommentCount(int id, int commentCount);
 }
--- src/main/java/io/github/jiajun2001/community/community/service/CommentService.java
+++ src/main/java/io/github/jiajun2001/community/community/service/CommentService.java
@@ -2,15 +2,28 @@ package io.github.jiajun2001.community.community.service;
 
 import io.github.jiajun2001.community.community.dao.CommentMapper;
 import io.github.jiajun2001.community.community.entity.Comment;
+import io.github.jiajun2001.community.community.util.CommunityConstant;
+import io.github.jiajun2001.community.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
 @Service
-public class CommentService {
+public class CommentService implements CommunityConstant {
+
     @Autowired
     private CommentMapper commentMapper;
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Autowired
+    private DiscussPostService discussPostService;
 
     public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit) {
         return commentMapper.selectCommentsByEntity(entityType, entityId, offset, limit);
@@ -18,5 +31,23 @@ public class CommentService {
 
     public int findCommentCount(int entityType, int entityId) {
         return commentMapper.selectCountByEntity(entityType, entityId);
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public int addComment(Comment comment) {
+        if (comment == null) {
+            throw new IllegalArgumentException("Comment cannot be empty!");
+        }
+        // Add comments
+        comment.setContent(HtmlUtils.htmlEscape(comment.getContent()));
+        comment.setContent(sensitiveFilter.filter(comment.getContent()));
+        int rows = commentMapper.insertComment(comment);
+
+        // Update the number of comments (when it is a comment to the post)
+        if (comment.getEntityType() == CommunityConstant.ENTITY_TYPE_POST) {
+            int count = commentMapper.selectCountByEntity(comment.getEntityType(), comment.getEntityId());
+            discussPostService.updateCommentCount(comment.getEntityId(), count);
+        }
+        return rows;
     }
 }
