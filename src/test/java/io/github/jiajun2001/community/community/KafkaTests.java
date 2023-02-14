73243c90cec44649b25e6ced92da0e745bfd8bf1
Jiajun-github-io <1372207033@qq.com>
2023-02-14 16:26:33 +1030

    Integrate Kafka into Spring

---
+++ src/test/java/io/github/jiajun2001/community/community/KafkaTests.java
@@ -0,0 +1,48 @@
+package io.github.jiajun2001.community.community;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+
+public class KafkaTests {
+
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
+    @Test
+    public void testKafka() throws InterruptedException {
+        kafkaProducer.sendMessage("test", "Hello World");
+        kafkaProducer.sendMessage("test", "Are you okay?");
+
+        Thread.sleep(1000 * 10);
+
+    }
+}
+
+@Component
+class KafkaProducer {
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    public void sendMessage(String topic, String content) {
+        kafkaTemplate.send(topic, content);
+    }
+}
+
+@Component
+class KafkaConsumer {
+
+    @KafkaListener(topics = {"test"})
+    public void handMessage(ConsumerRecord record) {
+        System.out.println(record.value());
+    }
+}
